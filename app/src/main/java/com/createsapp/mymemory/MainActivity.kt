e930ea081bfb42c8d327f736a12be6a15b1fc731
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-03-20 14:09:49 +0530

    Add the BoardSize enum+create icons

--- app/src/main/java/com/createsapp/mymemory/MainActivity.kt
+++ app/src/main/java/com/createsapp/mymemory/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.createsapp.mymemory.model.BoardSize
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
+
+    private var boardSize: BoardSize = BoardSize.EASY
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,8 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize)
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager(this, 2)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 }
